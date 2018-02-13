9acb59e277dfe1d9cee6be5473bfb361bd093177
Scott Daniel <scottdaniel25@gmail.com>
2018-02-13 14:44:04 -0700

    different software

--- scripts/setup.py
+++ scripts/setup.py
@@ -62,7 +62,7 @@ def installTools():
     conda.cli.main('conda', 'install', '-c', 'bioconda', '-y', 'bowtie2')
     conda.cli.main('conda', 'install', '-c', 'bioconda', '-y', 'samtools')
     conda.cli.main('conda', 'install', '-c', 'bioconda', '-y', 'bcftools')
-    conda.cli.main('conda', 'install', '-c', 'bioconda', '-y', 'khmer')
+    conda.cli.main('conda', 'install', '-c', 'bioconda', '-y', 'snpeff')
 
 
 checkConda()
